e5348d6006c293e72441767adddf8d675fe2986d
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2024-01-30 18:10:51 +0900

    비명시적(자동) 애니메이션 완료

--- lib/screens/explicit_animation_screen.dart
+++ lib/screens/explicit_animation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class ExplicitAnimationScreen extends StatefulWidget {
   const ExplicitAnimationScreen({super.key});
@@ -10,7 +11,18 @@ class ExplicitAnimationScreen extends StatefulWidget {
       _ExplicitAnimationScreenState();
 }
 
-class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen> {
+class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController =
+      AnimationController(vsync: this);
+  @override
+  void initState() {
+    super.initState();
+    Ticker((elapsed) {
+      print(elapsed);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
