83cf13dfb6306793dbb387e9b2889b3293cee1bc
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2024-02-26 15:40:00 +0900

    flutter_animate 완료

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class TestScreen extends StatefulWidget {
   static String routeName = "testscreen";
@@ -9,89 +10,62 @@ class TestScreen extends StatefulWidget {
   State<TestScreen> createState() => _TestScreenState();
 }
 
-class _TestScreenState extends State<TestScreen>
-    with SingleTickerProviderStateMixin {
-  //애니메이션 컨트롤러
-  late final AnimationController _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-      reverseDuration: const Duration(seconds: 1));
-
-  //trans를 위한 Animation<Offset>
-  late final Animation<Offset> _trans =
-      Tween(begin: const Offset(0, 0), end: const Offset(0.5, 0)).animate(
-          //애니메이션에 커브감 주기
-          CurvedAnimation(
-              parent: _animationController,
-              curve:
-                  //AnimationController 진행률이 50% ~ 100% 일때 수행
-                  const Interval(0.5, 1.0,
-                      curve: Curves.fastLinearToSlowEaseIn)));
-
-  //scale을 위한 Animation<Offset>
-  late final Animation<double> _scale =
-      Tween(begin: 1.0, end: 0.6).animate(CurvedAnimation(
-          parent: _animationController,
-          curve:
-              //AnimationController 진행률이 0% ~ 50% 일때 수행
-              const Interval(0.0, 0.5, curve: Curves.fastOutSlowIn)));
-
-  //클릭 했을 때 애니메이션효과 실행
-  void onTapforward() {
-    _animationController.forward();
-  }
-
-  //클릭 했을 때 애니메이션효과 거꾸로 실행
-  void onTapReverse() {
-    _animationController.reverse();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
+class _TestScreenState extends State<TestScreen> {
+  final List<MaterialColor> colorList = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.lightGreen,
+    Colors.lightBlue,
+    Colors.indigo,
+    Colors.deepPurple,
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            leading: IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: onTapReverse,
-            ),
-          ),
-          backgroundColor: Colors.blue,
-          body: const Center(
-            child: Text("첫번쨰 Stack 화면"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: AnimateList(
+                    onInit: (controller) {},
+                    onPlay: (controller) {},
+                    onComplete: (controller) {
+                      controller.reverse();
+                    },
+                    effects: [
+                      const FlipEffect(
+                          begin: 0,
+                          end: 1,
+                          direction: Axis.vertical,
+                          duration: Duration(milliseconds: 500)),
+                      const ScaleEffect(
+                          begin: Offset(1, 1), end: Offset(1.5, 1.5))
+                    ],
+                    interval: 500.ms,
+                    children: [
+                      for (int i = 0; i < colorList.length; i++)
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Container(
+                            height: 70,
+                            width: 350,
+                            decoration: BoxDecoration(
+                                color: colorList[i],
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(25))),
+                          ),
+                        ),
+                    ]),
+              )
+            ],
           ),
         ),
-        SlideTransition(
-          position: _trans,
-          child: ScaleTransition(
-            scale: _scale,
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.menu),
-                    onPressed: onTapforward,
-                  )
-                ],
-              ),
-              backgroundColor: Colors.green,
-              body: const Center(
-                child: Text("두번째 Stack 화면"),
-              ),
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
